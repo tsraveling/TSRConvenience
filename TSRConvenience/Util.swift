e192e915ea6056b8e2f24901ebc8318bd564778d
tsraveling <tsraveling@gmail.com>
2016-09-08 19:03:32 -0500

    Fixed Util

--- TSRConvenience/Util.swift
+++ TSRConvenience/Util.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-func delay(delay: Double, closure: ()->()) {
+public func delay(delay: Double, closure: ()->()) {
     dispatch_after(
         dispatch_time(
             DISPATCH_TIME_NOW,
